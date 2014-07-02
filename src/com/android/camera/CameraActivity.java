0ee3faeb3503369e0f5795c139238f97cbe18f17
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2014-07-02 11:59:05 +0530

    Camera: open Camera and start Preview in a seperate thread.
    Start openCamera and start preview in a separate thread so that
    UI initialization and preview can run parallely. This is to
    improve camera cold start latency.
    
    Change-Id: I57af45bc90a4db1630c73449d543c3ff0992e55c

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -183,6 +183,7 @@ public class CameraActivity extends Activity
     private ViewGroup mUndoDeletionBar;
     private boolean mIsUndoingDeletion = false;
     private boolean mIsEditActivityInProgress = false;
+    protected boolean mIsModuleSwitchInProgress = false;
 
     private Uri[] mNfcPushUris = new Uri[1];
 
@@ -1469,6 +1470,7 @@ public class CameraActivity extends Activity
             return;
         }
 
+        mIsModuleSwitchInProgress = true;
         CameraHolder.instance().keep();
         closeModule(mCurrentModule);
         setModuleFromIndex(moduleIndex);
@@ -1483,6 +1485,7 @@ public class CameraActivity extends Activity
         // starts up.
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.edit().putInt(CameraSettings.KEY_STARTUP_MODULE_INDEX, moduleIndex).apply();
+        mIsModuleSwitchInProgress = false;
     }
 
     /**
